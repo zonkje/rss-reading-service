c80fe6c01a0228a71d8c202dc3a0b0eeec9dca5e
szymek <szymon.radziszewski93@gmail.com>
2026-03-24 09:15:18 +0100

    init service

---
+++ src/main/java/com/szymek/rss_reading_service/RssReadingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.szymek.rss_reading_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssReadingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssReadingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szymek/rss_reading_service/RssReadingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymek.rss_reading_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssReadingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
